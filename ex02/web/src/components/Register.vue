71d9e9e83c88cfc78e7f866fe9533385e4f57c16
EIV Lina <94361925+linaEiv@users.noreply.github.com>
2022-04-06 00:57:34 +0700

    Update Register.vue

--- ex02/web/src/components/Register.vue
+++ ex02/web/src/components/Register.vue
@@ -50,17 +50,13 @@ body {
   font-family: Arial, Helvetica, sans-serif;
   background-color: black;
 }
-
 * {
   box-sizing: border-box;
 }
-
 /* Add padding to containers */
 .container {
   padding: 16px;
-  background-color: white;
 }
-
 /* Full-width input fields */
 input[type="text"],
 input[type="password"] {
@@ -71,19 +67,16 @@ input[type="password"] {
   border: none;
   background: #f1f1f1;
 }
-
 input[type="text"]:focus,
 input[type="password"]:focus {
   background-color: #ddd;
   outline: none;
 }
-
 /* Overwrite default styles of hr */
 hr {
   border: 1px solid #f1f1f1;
   margin-bottom: 25px;
 }
-
 /* Set a style for the submit button */
 .registerbtn {
   background-color: #04aa6d;
@@ -95,49 +88,17 @@ hr {
   width: 100%;
   opacity: 0.9;
 }
-
 .registerbtn:hover {
   opacity: 1;
 }
-
 /* Add a blue text color to links */
 a {
   color: dodgerblue;
 }
-
 /* Set a grey background color and center the text of the "sign in" section */
 .signin {
-  background-color: #f1f1f1;
+ 
   text-align: center;
 }
 </style>
 
-<script>
-import router from "../router"
-import axios from "axios"
-export default {
-  name: "Login",
-  methods: {
-    login: (e) => {
-      e.preventDefault()
-      let email = "user@email.com"
-      let password = "password"
-      let login = () => {
-        let data = {
-          email: email,
-          password: password
-        }
-        axios.post("/api/login", data)
-          .then((response) => {
-            console.log("Logged in")
-            router.push("/dashboard")
-          })
-          .catch((errors) => {
-            console.log("Cannot log in")
-          })
-      }
-      login()
-    }
-  }
-}
-</script>
